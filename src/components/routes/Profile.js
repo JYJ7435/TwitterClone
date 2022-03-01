a16bdfef55303387a66037761ae709cfa0ff2c26
JangYoungJu <ju7435@naver.com>
2022-03-01 15:04:49 +0900

    LogOut Setup

--- src/components/routes/Profile.js
+++ src/components/routes/Profile.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { firebaseAuth } from "../../firebase";
 
 function Profile(props) {
-  return <div>Profile</div>;
+  const history = useHistory();
+
+  const onLogOutClick = () => {
+    firebaseAuth.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 }
 
 export default Profile;
